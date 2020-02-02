2f1f6f61280e39258e0eb7a7df6cb3f1651b773d
cjd <cjd@DESKTOP-UATPGSM.local>
2020-02-02 10:07:22 -0800

    dbconnection: added class file loginview: setup of login view .fxml: buttons, fields, labels

---
+++ src/Model/DBConnection.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author cjd
+ */
+
+public class DBConnection {
+    
+//    private ObservableList<Appointment> Appointments;
+//    private ObservableList<Customer> Customers;
+//    private ObservableList<Address> Addresses;
+//    private ObservableList<City> Cities;
+//    private ObservableList<Country> Countries;
+//    
+
+}
