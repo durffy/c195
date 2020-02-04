49a82c931546c48628eb5fcde2b6b94e503175c7
Connor Duffy <connor_duffy@hotmail.com>
2020-02-04 08:24:14 -0800

    - Modifying DBConnect for the connection to the DB

--- src/Model/DBConnection.java
+++ src/Model/DBConnection.java
@@ -5,20 +5,37 @@
  */
 package Model;
 
-import javafx.collections.ObservableList;
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-/**
- *
- * @author cjd
- */
+public class DBConnection{
+    private static final String databaseName="";
+    private static final String DB_URL="jdbc:mysql://52.206.157.109/" + databaseName;
+    private static final String username="";
+    private static final String password="";
+    private static final String driver="com.mysql.jdbc.Driver";
 
-public class DBConnection {
-    
+    static Connection connection;
+  
+    public static void makeConnection() throws ClassNotFoundException, SQLException, Exception{
+
+      Class.forName(driver);
+      connection = DriverManager.getConnection(DB_URL, username, password);
+
+      System.out.println("Connection Successful!");
+
+    }
+
+    public static void closeConnection() throws ClassNotFoundException, SQLException, Exception{
+      connection.close();
+      System.out.println("Connection Closed!");
+    }
+  
+}  
 //    private ObservableList<Appointment> Appointments;
 //    private ObservableList<Customer> Customers;
 //    private ObservableList<Address> Addresses;
 //    private ObservableList<City> Cities;
 //    private ObservableList<Country> Countries;
 //    
-
-}
