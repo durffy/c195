dfc529be31dd2a5188f25762228318414bb85fd7
Connor Duffy <connor_duffy@hotmail.com>
2020-02-05 07:53:54 -0800

    - modifying login view to load the calendar view

--- src/Controller/LoginViewController.java
+++ src/Controller/LoginViewController.java
@@ -5,9 +5,18 @@
  */
 package Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,12 +25,44 @@ import javafx.fxml.Initializable;
  */
 public class LoginViewController implements Initializable {
 
+    @FXML private TextField TextFieldUsername;
+    @FXML private PasswordField PasswordField;
+    @FXML private Button ButtonLogin;
+    @FXML private Button ButtonCancel;
+    @FXML private ToggleGroup ToggleGroupLanguage;
+    @FXML private RadioButton RadioButtonEnglish;
+    @FXML private RadioButton RadioButtonSpanish;
+    
+    
+    public void clickButtonLogin(ActionEvent event) throws IOException{
+        
+        //check username and password against the db
+        //if true, load the Calendarview
+        if(true){
+            Parent root = FXMLLoader.load(getClass().getResource("/View/CalendarView.fxml"));
+            Scene scene = new Scene(root);
+            
+            Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            window.setScene(scene);
+            window.show();
+        }
+    }
+    
+    public void clickButtonCancel(){
+        
+        //prompt are you sure
+        //close program
+        
+    }
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+    
+    
     
 }
