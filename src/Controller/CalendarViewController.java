956ecea73a26dc8162b8ec93f2878e6387656928
Connor Duffy <connor_duffy@hotmail.com>
2020-02-05 07:57:31 -0800

    - button control added for customer view

--- src/Controller/CalendarViewController.java
+++ src/Controller/CalendarViewController.java
@@ -5,9 +5,16 @@
  */
 package Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,6 +23,20 @@ import javafx.fxml.Initializable;
  */
 public class CalendarViewController implements Initializable {
 
+    
+    
+    public void clickButtonCustomerView(ActionEvent event)throws IOException{
+        
+
+        Parent root = FXMLLoader.load(getClass().getResource("/View/CustomerView.fxml"));
+        Scene scene = new Scene(root);
+
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+
+    }
+    
     /**
      * Initializes the controller class.
      */
