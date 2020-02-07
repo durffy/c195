e6718cefbc63e76ccb46f4d19a94602781d0f693
Connor Duffy <connor_duffy@hotmail.com>
2020-02-07 08:02:10 -0800

    - adding controller methods to ModifyAppointmentViewController.java - fixing .fxml

--- src/Controller/ModifyAppointmentViewController.java
+++ src/Controller/ModifyAppointmentViewController.java
@@ -5,9 +5,21 @@
  */
 package Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,6 +28,48 @@ import javafx.fxml.Initializable;
  */
 public class ModifyAppointmentViewController implements Initializable {
 
+    @FXML private TextField TextFieldAppointmentID;
+    @FXML private TextField TextFieldUserName;
+    @FXML private TextField TextFieldTitle;
+    @FXML private TextField TextFieldLocation;
+    @FXML private TextField TextFieldContact;
+    @FXML private TextField TextFieldType;
+    
+    @FXML private DatePicker DatePickerStart;
+    @FXML private DatePicker DatePickerEnd;
+    
+    @FXML private TextArea TextAreaDescription;
+    
+    @FXML private Button ButtonSave;
+    @FXML private Button ButtonCancel;
+    
+    public void loadCalendarView(ActionEvent event)throws IOException{
+        
+        Parent root = FXMLLoader.load(getClass().getResource("/View/CalendarView.fxml"));
+        Scene scene = new Scene(root);
+
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+
+    }
+    
+    public void clickButtonSave(ActionEvent event) throws IOException{
+        
+        //check the input
+        //add the record
+        loadCalendarView(event);
+        
+    }
+    
+    
+    public void clickButtonCancel(ActionEvent event) throws IOException{
+        
+        loadCalendarView(event);
+        
+    }
+    
+    
     /**
      * Initializes the controller class.
      */
