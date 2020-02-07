a16dae6bed614c387e4866fe6f607c383ea1276f
Connor Duffy <connor_duffy@hotmail.com>
2020-02-07 07:22:38 -0800

    - adding CustomerView methods

--- src/Controller/CustomerViewController.java
+++ src/Controller/CustomerViewController.java
@@ -5,16 +5,23 @@
  */
 package Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -27,9 +34,9 @@ public class CustomerViewController implements Initializable {
     @FXML private TextField TextFieldCustomerName;
     
     @FXML private Button ButtonGoBack;
-    @FXML private Button ButtonCancel;
-    @FXML private Button ButtonRemove;
     @FXML private Button ButtonAddModify;
+    @FXML private Button ButtonRemove;
+    @FXML private Button ButtonCancel;
     
     @FXML private MenuButton MenuButtonAddress;
     
@@ -43,12 +50,40 @@ public class CustomerViewController implements Initializable {
     @FXML private TableColumn TableCustomerColumnAddress;
     @FXML private TableColumn TableCustomerColumnActive;
     
+    
+    public void clickButtonGoBack(ActionEvent event) throws IOException{
+        
+            Parent root = FXMLLoader.load(getClass().getResource("/View/CalendarView.fxml"));
+            Scene scene = new Scene(root);
+            
+            Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            window.setScene(scene);
+            window.show();
+            
+    }
+    
+    public void clickButtonAddModify(ActionEvent event) throws IOException{
+        //todo: add if no customer record is selected
+        //todo: modify if customer record is selected
+    }
+    
+    public void clickButtonRemove(ActionEvent event) throws IOException{
+        //todo: remove record if customer record is selected
+        
+    }        
+    
+    public void clickButtonCancel(ActionEvent event) throws IOException{
+        //todo: deselect if customer record is selected
+        
+    }
+    
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO: load the Address Choices to the MenuButton
+        // TODO: load the Address Choices to the MenuButtonAddress
  
     }    
     
